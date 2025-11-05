b412793a42c3cc0bc800a53ffdaf7ac67ae42bff
ArchdukeRoy <royvallecera.01@gmail.com>
2025-11-06 01:14:23 +1030

    BPM, functional
    BPM number added, currently functional but needs to be refactored into utils and components

--- src/App.js
+++ src/App.js
@@ -31,6 +31,9 @@ export default function StrudelDemo() {
     //initialise useState variable
     const [globalEditor, setEditor] = useState(null);
 
+    //
+    const [cpsValue, setCpsValue] = useState(140); // default BPM
+
 useEffect(() => {
 
     if (!hasRun.current) {
@@ -66,7 +69,7 @@ useEffect(() => {
                 });
 
         //feed intialised editor variable into useState
-        //cann now use "globalEditor"
+        //can now use "globalEditor"
         setEditor(editor);
             
         document.getElementById('proc').value = stranger_tune
@@ -78,13 +81,12 @@ useEffect(() => {
 
 return (
     <div>
-
-        <div className="App container">
-            <div className="bg-light py-1 mb-2">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+            <div className="container-fluid d-flex justify-content-between align-items-center">
                 <h2 className="text-center">Strudel Demo</h2>
                 {/*<!-- Button trigger modal -->*/}
                 <button type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">Settings</button>
-               {/* <!-- Modal -->*/}
+                {/* <!-- Modal -->*/}
                 <div class="modal fade" id="exampleModalCenter" tabindex="-1" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                     <div class="modal-dialog modal-dialog-centered" role="document">
                         <div class="modal-content">
@@ -113,12 +115,12 @@ return (
                     </div>
                 </div>
             </div>
-        </div>
+        </nav>
 
         <main>
             <div className="container-fluid">
-                <div className="row">
-                    {/*process box*/}
+                <div className="row g-0">
+                    {/*process*/}
                     <div className="col-md-6 d-flex flex-column border-end">
                         <div className="bg-light border-bottom p-2 d-flex justify-content-between align-items-center">
                             <span className="fw-bold">Pre-Process text:</span>
@@ -131,7 +133,7 @@ return (
                         </div>
                         <textarea className="form-control flex-grow-1 rounded-0" id="proc" style={{ maxHeight: '400px', overflowY: 'auto', overflowX: 'auto', whiteSpace: 'pre' }}></textarea>
                     </div>
-                    {/* output box*/}
+                    {/*output*/}
                     <div className="col-md-6 d-flex flex-column">
                         <div className="bg-light border-bottom p-2 d-flex justify-content-between align-items-center">
                             <span className="fw-bold">Output:</span>
@@ -148,72 +150,90 @@ return (
                 </div>
             </div>
             <br></br>
-            <div className="row">
-                <div className="col-md-4">
-                    <div className="form-check">
-                    {/*Buttons*/}
+            <nav className="navbar" style={{ backgroundColor: '#f0f0f0' }}>
+                <div className="container-fluid d-flex align-items-center gap-3 flex-wrap justify-content-start">
+                    {/*Radio*/}
+                    <div className="form-check d-flex align-items-center gap-3">
                         <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" onChange={ProcAndPlay} defaultChecked />
-                        <label className="form-check-label" htmlFor="flexRadioDefault1">
-                            p1: ON
-                        </label>
+                        <label className="form-check-label mb-0" htmlFor="flexRadioDefault1">p1: ON</label>
                     </div>
-                    <div className="form-check">
+                    <div className="form-check d-flex align-items-center gap-3">
                         <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onChange={ProcAndPlay} />
-                        <label className="form-check-label" htmlFor="flexRadioDefault2">
-                            p1: HUSH
-                        </label>
-                       {/* Volume*/}
-                        <label for="volumeRange" className="form-label">Example range</label>
-                        <input type="range" className="form-range" min="0" max="100" id="volumeRange" />
-                        {/*Speed*/}
-                        <div className="dropdown">
-                            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Speed
-                            </button>
-                            <ul className="dropdown-menu">
-                                <li><button className="dropdown-item" type="button">0.50x</button></li>
-                                <li><button className="dropdown-item" type="button">0.75x</button></li>
-                                <li><button className="dropdown-item" type="button">1.00x</button></li>
-                                <li><button className="dropdown-item" type="button">1.50x</button></li>
-                                <li><button className="dropdown-item" type="button">1.75x</button></li>
-                            </ul>
-                        </div>
-                        {/*Instruments*/}
-                        <div className="accordion accordion-flush" id="accordionFlushExample">
-                            <div className="accordion-item">
-                                <h2 className="accordion-header">
-                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                                        Piano
-                                    </button>
-                                </h2>
-                                <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                                    <div className="accordion-body"> bvhjcvgh </div>
-                                </div>
-                            </div>
-                            <div className="accordion-item">
-                                <h2 className="accordion-header">
-                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                                        Drums
-                                    </button>
-                                </h2>
-                                <div id="flush-collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                                    <div className="accordion-body"> bhbgjyhubv </div>
-                                </div>
-                            </div>
-                            <div className="accordion-item">
-                                <h2 className="accordion-header">
-                                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                                        Bass
-                                    </button>
-                                </h2>
-                                <div id="flush-collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                                    <div className="accordion-body"> jnbjhbh</div>
-                                </div>
-                            </div>
-                        </div>
+                        <label className="form-check-label mb-0" htmlFor="flexRadioDefault2">p1: HUSH</label>
                     </div>
+                    {/*Volume*/}
+                    <div className="d-flex align-items-center gap-3" style={{ minWidth: '180px' }}>
+                        <label htmlFor="volumeRange" className="form-label mb-0">Volume: </label>
+                        <input type="range" className="form-range" min="0" max="100" id="volumeRange" style={{ height: '30px' }} />
+                    </div>
+                    {/*Speed*/}
+                    <div className="dropdown">
+                        <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Speed</button>
+                        <ul className="dropdown-menu">
+                            <li><button className="dropdown-item" type="button">0.50x</button></li>
+                            <li><button className="dropdown-item" type="button">0.75x</button></li>
+                            <li><button className="dropdown-item" type="button">1.00x</button></li>
+                            <li><button className="dropdown-item" type="button">1.50x</button></li>
+                            <li><button className="dropdown-item" type="button">1.75x</button></li>
+                        </ul>
+                    </div>
+                    {/*Instruments*/}
+                    <div className="dropdown">
+                        <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Instruments</button>
+                        <ul className="dropdown-menu p-2" style={{ minWidth: '250px' }}>
+                            {/*onClick to stop the according from closing drowdown on click*/}
+                            <div className="accordion accordion-flush" id="accordionFlushExample" onClick={e => e.stopPropagation()}>
+                                <div className="accordion-item">
+                                    <h2 className="accordion-header">
+                                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">Piano</button>
+                                    </h2>
+                                    <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                                        <div className="accordion-body">bvhjcvgh</div>
+                                    </div>
+                                </div>
+                                <div className="accordion-item">
+                                    <h2 className="accordion-header">
+                                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">Drums</button>
+                                    </h2>
+                                    <div id="flush-collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                                        <div className="accordion-body">bhbgjyhubv</div>
+                                    </div>
+                                </div>
+                                <div className="accordion-item">
+                                    <h2 className="accordion-header">
+                                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">Bass</button>
+                                    </h2>
+                                    <div id="flush-collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                                        <div className="accordion-body">jnbjhbh</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </ul>
+                    </div>
+                    {/*BPM control*/}
+                    <div className="d-flex align-items-center gap-2 mb-2">
+                        <label htmlFor="bpmIn" className="mb-0">BPM:</label>
+                        <input type="number" id="bpmIn" className="form-control" value={cpsValue} onChange={(e) => setCpsValue(Number(e.target.value))} style={{ width: '80px' }} />
+                        <button className="btn btn-outline-dark"
+                            onClick={() => {
+                                //insert  value in pre-process
+                                const textarea = document.getElementById('proc');
+                                //if check to stop errors if pre process is empty
+                                if (textarea) {
+                                    //break process string into lines so can read 
+                                    const lines = textarea.value.split('\n');
+                                    //if a string in the array starts with setcps then replace it with the inputted value
+                                    const newLines = lines.map(line => line.startsWith('setcps') ? `setcps(${cpsValue}/60/4)` : line);
+                                    //put it back to 1 string then feed into preprocess area
+                                    textarea.value = newLines.join('\n');
+                                }
+                            }}>
+                            Update CPS
+                        </button>
+                    </div>
+
                 </div>
-            </div>
+            </nav>
             <canvas id="roll"></canvas>
         </main >
     </div >
